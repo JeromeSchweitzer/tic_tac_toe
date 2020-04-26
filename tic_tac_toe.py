3e3d6ecf3b76141a8dafdf7814af358cd7027fed
Jerome Schweitzer <jeromeschweitzer@Jeromes-MBP.attlocal.net>
2020-04-26 02:49:17 -0400

    adding markers, finding winner

--- tic_tac_toe.py
+++ tic_tac_toe.py
@@ -1,10 +1,16 @@
 import numpy as np
 
+
 class TicTacToe:
+
 	def __init__(self):
-		self.board = np.zeros((3,3))
+		self.board = np.empty((3,3))
+		self.board[:] = np.nan			# Initializing to NaN values for ease of checking winner
+		self.winner = None
+
 
 	def __repr__(self):
+		# TODO: consider not using for loops
 		board_str = ""
 		for row_idx, row in enumerate(self.board):
 			if row_idx:
@@ -15,3 +21,23 @@ class TicTacToe:
 				board_str += ' ' + ('X' if col == 1 else 'O' if col == 2 else ' ')
 			board_str += '\n'
 		return board_str
+
+
+	def place(self, row, col, marker):
+		if not np.isnan(self.board[row][col]):
+			return False
+		self.board[row][col] = marker
+		self.check_win()
+		return True
+
+
+	def check_win(self):
+		# Below, iterating through the rows, columns, diagonals
+		for line in np.row_stack((self.board, self.board.T, np.diag(self.board), np.diag(self.board))):
+			line_unique_count = np.unique(line).size			# line=[NaN, NaN, NaN] will result in 3
+			if line_unique_count == 1:
+				self.winner = line[0]
+				return True
+		return False
+		
+
